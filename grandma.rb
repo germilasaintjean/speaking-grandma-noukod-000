3538a8bc981b3aa8e9b517956d3924fa1447a381
Saint-Jean Germila <germila@noukod.com>
2018-09-21 14:51:51 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -6,7 +6,7 @@ def speak_to_grandma phrase
       return "HUH?! SPEAK UP, SONNY!"
     end
    else
-      "#{phrase}" == "Whats?"
+      "#{phrase}" == "WHAT?"
      return "NO, NOT SINCE 1938!"
    end
 end
