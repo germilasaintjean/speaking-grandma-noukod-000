1506f65e694466943e2602e98ecc027cffe7d66b
Saint-Jean Germila <germila@noukod.com>
2018-09-21 14:27:10 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,9 @@
 def speak_to_grandma( phrase ="I LOVE YOU GRANDMA!")
     if "#{phrase}".upcase
-    puts "HUH?! SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
+  end
     elsif
-     puts "NO, NOT SINCE 1938!"
+     return "NO, NOT SINCE 1938!"
    else
     puts "I LOVE YOU TOO PUMPKIN!"
   end
