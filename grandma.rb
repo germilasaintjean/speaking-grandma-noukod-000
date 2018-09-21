9caaacd91b0f3e57c51284b4b145601001520618
Saint-Jean Germila <germila@noukod.com>
2018-09-21 14:47:50 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 def speak_to_grandma phrase
-    if "#{phrase}" =="I LOVE YOU GRANDMA!"
-      puts "I LOVE YOU TOO PUMPKIN!"
+    if "#{phrase}" == "I LOVE YOU GRANDMA!"
+      return "I LOVE YOU TOO PUMPKIN!"
    elsif
       phrase =="It's you Granma"
       puts "HUH?! SPEAK UP, SONNY!"
